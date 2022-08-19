0d8be8dbfba9459a86ec413c8f6d1aa152698123
yydswaps <ringsfrog19@gmail.com>
2022-08-19 15:22:25 +0800

    first commit

---
+++ src/constants/lists.ts
@@ -0,0 +1,7 @@
+// the Uniswap Default token list lives here
+export const DEFAULT_TOKEN_LIST_URL = 'https://raw.githubusercontent.com/yydswaps/tokenlistswap/main/yydswap.tokenlist.json'
+
+export const DEFAULT_LIST_OF_LISTS: string[] = [
+  DEFAULT_TOKEN_LIST_URL,
+  'https://gateway.pinata.cloud/ipfs/QmdAZYFimZFGTfx1vCxXKv27TCyRkzjM6X3M5ivuWZkzVM'
+]
